ec77db658473b4fceb206e5f14989bdffb83422f
Mohamed Zayan <m7mdzayan@gmail.com>
2020-02-08 14:51:07 +0200

    core types are explained

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log("Hello TypeScript");
-var a = 5;
+function add(a, b, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + " " + (a + b));
+    }
+    else {
+        return a + b;
+    }
+}
+var num1 = 5;
+var num2 = 10;
+var printResult = true;
+var resultPhrase = "Result is";
+add(num1, num2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,3 +1,14 @@
-console.log("Hello TypeScript");
+function add(a: number, b: number, showResult: boolean, phrase: string) {
+  if (showResult) {
+    console.log(`${phrase} ${a + b}`);
+  } else {
+    return a + b;
+  }
+}
 
-let a: number = 5;
+const num1 = 5;
+const num2 = 10;
+let printResult = true;
+const resultPhrase = "Result is";
+
+add(num1, num2, printResult, resultPhrase);
